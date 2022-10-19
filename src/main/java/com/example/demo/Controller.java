27bd9a692f2a31bb47bc9c9578c80288b7d55a3f
Heisenberg0803 <rafael.nogueira.andade@escola.pr.gov.br>
2022-10-18 21:54:32 -0300

    Banco de dados em Java

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value="/cliente/v1/")
+
+public class Controller {
+    @Autowired
+    Repository repository;
+
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente){
+        Cliente clienteSaved = repository.save(cliente);
+        return clienteSaved;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Optional<Cliente> getClienteById(@PathVariable Long id){
+        Optional<Cliente> clienteReturned = repository.findById(id);
+        return clienteReturned;
+    }
+
+
+
+}
